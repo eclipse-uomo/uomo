a666f84f784e24ae77ac42cf4b9b74de3fe25b1f
Werner Keil <werner.keil@gmx.net>
2010-11-25 23:28:01 +0530

    Updated Business demos

--- bundles/org.eclipse.uomo.business/src/test/java/org/eclipse/uomo/business/money/MoneyDemo.java
+++ bundles/org.eclipse.uomo.business/src/test/java/org/eclipse/uomo/business/money/MoneyDemo.java
@@ -11,12 +11,13 @@
 package org.eclipse.uomo.business.money;
 
 // Constants (Java 5 static import)
-import static org.eclipse.uomo.units.SI.Prefix.KILO;
-import static org.eclipse.uomo.units.SI.METRE;
 import static org.eclipse.uomo.units.USCustomary.GALLON_LIQUID;
 import static org.eclipse.uomo.units.USCustomary.LITER;
 import static org.eclipse.uomo.units.USCustomary.MILE;
+import static org.eclipse.uomo.units.SI.Prefix.KILO;
+import static org.eclipse.uomo.units.SI.METRE;
 import static org.eclipse.uomo.business.money.CurrencyUnit.EUR;
+import static org.eclipse.uomo.business.money.CurrencyUnit.GBP;
 import static org.eclipse.uomo.business.money.CurrencyUnit.USD;
 
 import org.eclipse.uomo.business.money.CurrencyConverter;
@@ -27,7 +28,7 @@ import org.eclipse.uomo.units.impl.quantity.LengthAmount;
 
 /**
  * @author Werner Keil
- * @version 0.9.5, $Date: 2010-09-11 23:59:41 +0200 (Sa, 11 Sep 2010) $
+ * @version 0.9.6, $Date: 2010-09-11 23:59:41 +0200 (Sa, 11 Sep 2010) $
  */
 public class MoneyDemo {
 
@@ -41,33 +42,8 @@ public class MoneyDemo {
 		// Calculates the cost of a car trip in Europe for an American tourist.
 		// /////////////////////////////////////////////////////////////////////
 
-		// Use currency symbols instead of ISO-4217 codes.
-		// UnitFormat.getInstance().label(USD, "$"); // Use "$" symbol instead
-		// of currency code ("USD")
-		// UnitFormat.getInstance().label(EUR, "€"); // Use "€" symbol
-		// instead of currency code ("EUR")
-		// UnitFormat.getInstance().label(GBP, "£"); // Use "£" symbol instead
-		// of currency code ("GBP")
-		// UnitFormat.getInstance().label(INR, "Rp"); // Use "Rp" instead of
-		// currency code ("IRP")
-		// MeasureFormat currFormat = MeasureFormat.getCurrencyFormat();
-		// Sets exchange rates.
-		// Currency.setReferenceCurrency(USD);
-		// EUR.setExchangeRate(1.55); // 1.0 € = 1.4 $
-		// GBP.setExchangeRate(2); // 1.0 £ = 2.0 $
-		// INR.setExchangeRate(0.022); // 1.0Rp = ~0.022 $
 		@SuppressWarnings("unused")
 		CurrencyConverter converter1 = new CurrencyConverter(USD, EUR, 1.4);
-
-		// Calculates trip cost.
-		/*
-		 * Measureable<?> carMileage = Measure.valueOf(20,
-		 * MILE.divide(GALLON_LIQUID_US)); // 20 mi/gal. Measure<?> gazPrice =
-		 * Measure.valueOf(1.2, EUR.divide(LITER)); // 1.2 €/L Measure<Length>
-		 * tripDistance = Measure.valueOf(400, KILO(SI.METER)); // 400 km
-		 * Measureable<Money> tripCost =
-		 * tripDistance.divide(carMileage).times(gazPrice).to(USD);
-		 */
 
 		// Calculates trip cost.
 		BaseAmount carMileage = new BaseAmount(20,
