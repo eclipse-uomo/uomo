8794516042877c61c6bab3aa5e63a7dc218ad68d
Werner Keil <werner.keil@gmx.net>
2010-12-02 00:52:02 +0530

    Added units example (based on JSR-308 units demo)

---
+++ bundles/org.eclipse.uomo.units/src/test/java/org/eclipse/uomo/units/impl/UnitsExample.java
@@ -0,0 +1,21 @@
+package org.eclipse.uomo.units.impl;
+
+import org.unitsofmeasurement.quantity.Acceleration;
+import org.unitsofmeasurement.quantity.Length;
+import org.unitsofmeasurement.quantity.Time;
+import org.unitsofmeasurement.unit.Unit;
+
+import static org.eclipse.uomo.units.SI.*;
+
+public class UnitsExample {
+	  public static void main(String[] args) {
+	    Unit<Length> distance = METRE.multiply(5);
+	    Unit<Time> time = SECOND.multiply(10);
+	    @SuppressWarnings("unchecked")
+		Unit<Acceleration> speed = (Unit<Acceleration>) distance.divide(time);
+
+	    System.out.println("Speed = " + speed);
+
+//	    Unit<?> nonsense = distance.add(time);
+	  }
+}
