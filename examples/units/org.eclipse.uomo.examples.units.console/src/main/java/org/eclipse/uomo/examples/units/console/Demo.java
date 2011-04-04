45893b05c91ccdcf4a41b1aa2b6cefe1ff168740
Werner Keil <werner.keil@gmx.net>
2011-04-04 23:36:18 +0530

    ASSIGNED - bug 332369: Update Documentation https://bugs.eclipse.org/bugs/show_bug.cgi?id=332369

--- examples/units/org.eclipse.uomo.examples.units.console/src/main/java/org/eclipse/uomo/examples/units/console/Demo.java
+++ examples/units/org.eclipse.uomo.examples.units.console/src/main/java/org/eclipse/uomo/examples/units/console/Demo.java
@@ -1,14 +1,14 @@
 /**
- * Copyright (c) 2005, 2010, Werner Keil, Ikayzo and others.
+ * Copyright (c) 2005, 2011, Werner Keil, JScience and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Werner Keil, Ikayzo and others - initial API and implementation
+ *    Werner Keil, Martin Desruisseaux - initial API and implementation
  */
-package org.eclipse.uomo.units.impl;
+package org.eclipse.uomo.examples.units.console;
 
 import static org.eclipse.uomo.units.SI.*;
 import static org.eclipse.uomo.units.USCustomary.FOOT;
@@ -24,14 +24,13 @@ import org.unitsofmeasurement.quantity.Time;
 /**
  * @author <a href="mailto:desruisseaux@users.sourceforge.net">Martin Desruisseaux</a>
  * @author <a href="mailto:uomo@catmedia.us">Werner Keil</a>
- * @version 0.5 ($Revision: 210 $), $Date: 2010-02-25 23:34:46 +0100 (Do, 25 Feb 2010) $
+ * @version 0.6 ($Revision: 210 $), $Date: 2010-02-25 23:34:46 +0100 (Do, 25 Feb 2010) $
  */
 public class Demo {
 
 	private static Length getSomeLength() {
         return new LengthAmount(20, METRE);
     }
-
 	
 	private static IMeasure<Length> getMoreLength() {
         return new LengthAmount(20, METRE);
