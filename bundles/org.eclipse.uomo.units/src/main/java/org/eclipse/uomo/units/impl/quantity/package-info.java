5a176a999985c281bf4ff52474f64c8476d53428
keilw <werner.keil@gmx.net>
2013-05-20 18:41:24 +0200

    ASSIGNED - bug 332369: Update Documentation https://bugs.eclipse.org/bugs/show_bug.cgi?id=332369

--- bundles/org.eclipse.uomo.units/src/main/java/org/eclipse/uomo/units/impl/quantity/package-info.java
+++ bundles/org.eclipse.uomo.units/src/main/java/org/eclipse/uomo/units/impl/quantity/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2005, 2011, Werner Keil, Ikayzo and others.
+ * Copyright (c) 2005, 2013, Werner Keil and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
