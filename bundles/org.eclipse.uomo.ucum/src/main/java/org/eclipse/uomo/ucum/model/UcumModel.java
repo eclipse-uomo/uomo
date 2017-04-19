162f6bb0da30fe23c3a59a6d30d4507608a9d6a2
Werner Keil <werner.keil@gmx.net>
2017-04-19 21:01:05 +0200

    515344: Integrate new UCUM version
    Change-Id: Ic9024efa930c134c47870a5e0b369f79930f07ad
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=515344
    Signed-off-by: Werner Keil <werner.keil@gmx.net>

--- bundles/org.eclipse.uomo.ucum/src/main/java/org/eclipse/uomo/ucum/model/UcumModel.java
+++ bundles/org.eclipse.uomo.ucum/src/main/java/org/eclipse/uomo/ucum/model/UcumModel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Crown Copyright (c) 2006, 2013, Copyright (c) 2006, 2008 Kestral Computing P/L.
+ * Crown Copyright (c) 2006, 2013, Copyright (c) 2006, 2017 Kestral Computing P/L and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,25 +22,24 @@ import org.eclipse.uomo.core.IVersion;
 public class UcumModel implements IName, IVersion {
 
 	/**
-	 * version="1.8" 
+	 * UCUM version
 	 */
 	private final String version;
-	
+
 	/**
 	 * revision="$Revision: 1.1 $"
 	 */
-//	private final String revision;
-	
+	// private final String revision;
+
 	/**
 	 * date this revision was made public
 	 */
 	private final Date revisionDate;
-	
+
 	private final List<Prefix> prefixes = new ArrayList<Prefix>();
 	private final List<BaseUnit> baseUnits = new ArrayList<BaseUnit>();
 	private final List<DefinedUnit> definedUnits = new ArrayList<DefinedUnit>();
 
-	
 	/**
 	 * @param revision
 	 * @param revisionDate
@@ -48,68 +47,73 @@ public class UcumModel implements IName, IVersion {
 	public UcumModel(String version, String revision, Date revisionDate) {
 		super();
 		this.version = version;
-//		this.revision = revision;
+		// this.revision = revision;
 		this.revisionDate = revisionDate;
 	}
-	
+
 	public String getName() {
 		return "UCUM";
 	}
+
 	/**
 	 * @return the prefixes
 	 */
 	public List<Prefix> getPrefixes() {
 		return prefixes;
 	}
+
 	/**
 	 * @return the baseUnits
 	 */
 	public List<BaseUnit> getBaseUnits() {
 		return baseUnits;
 	}
+
 	/**
 	 * @return the units
 	 */
 	public List<DefinedUnit> getDefinedUnits() {
 		return definedUnits;
 	}
+
 	/**
 	 * @return the revision
 	 */
-//	public String getRevision() {
-//		return revision;
-//	}
-//	/**
-//	 * @param revision the revision to set
-//	 */
-//	public void setRevision(String revision) {
-//		this.revision = revision;
-//	}
+	// public String getRevision() {
+	// return revision;
+	// }
+	// /**
+	// * @param revision the revision to set
+	// */
+	// public void setRevision(String revision) {
+	// this.revision = revision;
+	// }
 	/**
 	 * @return the revisionDate
 	 */
 	public Date getRevisionDate() {
 		return revisionDate;
 	}
-//	/**
-//	 * @param revisionDate the revisionDate to set
-//	 */
-//	public void setRevisionDate(Date revisionDate) {
-//		this.revisionDate = revisionDate;
-//	}
+
+	// /**
+	// * @param revisionDate the revisionDate to set
+	// */
+	// public void setRevisionDate(Date revisionDate) {
+	// this.revisionDate = revisionDate;
+	// }
 	/**
 	 * @return the version
 	 */
 	public String getVersion() {
 		return version;
 	}
-//	/**
-//	 * @param version the version to set
-//	 */
-//	public void setVersion(String version) {
-//		this.version = version;
-//	}
-	
+	// /**
+	// * @param version the version to set
+	// */
+	// public void setVersion(String version) {
+	// this.version = version;
+	// }
+
 	public UcumUnit getUnit(String code) {
 		for (UcumUnit unit : getBaseUnits()) {
 			if (unit.getCode().equals(code))
@@ -121,7 +125,7 @@ public class UcumModel implements IName, IVersion {
 		}
 		return null;
 	}
-	
+
 	public BaseUnit getBaseUnit(String code) {
 		for (BaseUnit unit : getBaseUnits()) {
 			if (unit.getCode().equals(code))
